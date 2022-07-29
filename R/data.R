b4cc045c4a7d004e1fed38d0fad9266e8810527c
Chunxuan Shao <c.shao@dkfz-heidelberg.de>
2022-07-29 09:56:29 +0200

    feat: add example data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Fly embyro spatial data
+#'
+#' A gene by bin matrix, values are quantile normalized.
+#'
+#' @format A matrix
+#' @usage data(example_bin)
+"example_bin"
+
+#' Fly embyro single-cell RNAseq data
+#'
+#' A gene by cell matrix, values are normalized by read depth. See Nikos Karaiskos et al., (2017) for details.
+#'
+#' @format A sparse matrix
+#' @usage data(example_expr)
+"example_expr"
